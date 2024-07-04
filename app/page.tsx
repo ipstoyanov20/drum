6c47856c86c9533d7712abb44e994bc3a7754afb
Ivaylo Stoyanov <ipstoyanov20@codingburgas.bg>
2024-07-04 16:48:49 +0300

    Set a baisic

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,70 @@
-import Image from "next/image";
-
+import logo from "../public/vercel.svg";
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <nav className=''>
-        <p>About me</p>
-        <p>Pricing</p>
-        <p>Pay</p>
-      </nav>
-      <div className=''>
-        <p>About me</p>
-      </div>
-      <div className=''>
-        <p>Pricing</p>
-      </div>
-      <div className=''>
-        <p>Pay</p>
-      </div>
-      <footer className=''>
-        <div className=''>
-        <p>About me</p>
-        <p>Pricing</p>
-        <p>Pay</p>
-        </div>
-      </footer>
-    </main>
-  );
+		<main className="flex min-h-screen flex-col items-center ">
+			<Navbar />
+
+			<div className="hero bg-background min-h-screen">
+				<div className="hero-content flex-col lg:flex-row">
+					<img
+						src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
+						className="max-w-sm rounded-lg shadow-2xl"
+					/>
+					<div>
+						<h1 className="text-5xl font-bold">Box Office News!</h1>
+						<p className="py-6">
+							Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+							excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+							et a id nisi.
+						</p>
+						<button className="btn btn-primary">Get Started</button>
+					</div>
+				</div>
+			</div>
+
+			<div className="hero bg-background min-h-screen">
+				<div className="hero-content flex-col lg:flex-row-reverse">
+					<img
+						src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
+						className="max-w-sm rounded-lg shadow-2xl"
+					/>
+					<div>
+						<h1 className="text-5xl font-bold">Box Office News!</h1>
+						<p className="py-6">
+							Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+							excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+							et a id nisi.
+						</p>
+						<button className="btn btn-primary">Get Started</button>
+					</div>
+				</div>
+			</div>
+
+
+
+			<div className="hero bg-background min-h-screen">
+				<div className="hero-content flex-col lg:flex-row">
+					<img
+						src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
+						className="max-w-sm rounded-lg shadow-2xl"
+					/>
+					<div>
+						<h1 className="text-5xl font-bold">Box Office News!</h1>
+						<p className="py-6">
+							Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+							excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+							et a id nisi.
+						</p>
+						<button className="btn btn-primary">Get Started</button>
+					</div>
+				</div>
+			</div>
+
+
+
+			<Footer />
+		</main>
+	);
 }
---
+++ components/navbar.tsx
@@ -0,0 +1,38 @@
+function Navbar() {
+  return (
+		<div className="navbar bg-background">
+			<div className="flex-1">
+				<a className="btn btn-ghost text-xl">daisyUI</a>
+			</div>
+			<div className="flex-none">
+				<ul className="menu menu-horizontal px-1">
+					<div>
+						<li>
+							<a>About me</a>
+						</li>
+					</div>
+					<div>
+						<li>
+							<a>Pricing</a>
+						</li>
+					</div>
+					<li>
+						<details>
+							<summary>Parent</summary>
+							<ul className="bg-background rounded-t-none p-2">
+								<li>
+									<a>Link 1</a>
+								</li>
+								<li>
+									<a>Link 2</a>
+								</li>
+							</ul>
+						</details>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
+}
+
+export default Navbar
