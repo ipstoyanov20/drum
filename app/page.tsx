67a6df94f56baf0e8cd9513d24a78c02be1cea39
Ivaylo Stoyanov <ipstoyanov20@codingburgas.bg>
2024-07-11 12:00:04 +0300

    Add hreo section to be developed

--- app/page.tsx
+++ app/page.tsx
@@ -7,20 +7,22 @@ export default function Home() {
 			<Navbar />
 
 
-			<div className="hero bg-background min-h-screen">
-				<div className="hero-content flex-col lg:flex-row-reverse">
-					<img
+			<div className="hero grid place-items-start place-content-center bg-background min-h-screen">
+				<div className=" flex-col lg:flex-row-reverse">
+					{/* <img
 						src="darbuka.png"
 						className="max-w-sm rounded-lg shadow-2xl"
-					/>
-					<div>
-						<h1 className="text-5xl font-bold">Box Office News!</h1>
-						<p className="py-6">
-							Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-							excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-							et a id nisi.
+					/> */}
+					<div className="grid place-items-center place-content-start">
+						<p className="font-bold text-[#B59861] text-xl">
+						Unchanged. It was popularised in the 1960s with the release 	
 						</p>
-						<button className="btn btn-primary">Get Started</button>
+						<h1 className="text-9xl font-grotesk m-5 text-center"> My Rythm <br /> Your favourite song</h1>
+						<p className="m-5 grid place-content-center text-center text-lg text-[#B59861]">
+							Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda<br /> 
+							excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+						</p>
+						<button className="rounded-sm px-4 py-3 m-10 bg-[#B59861] text-[#3d2d0e] font-bold font-grotesk">Get Started</button>
 					</div>
 				</div>
 			</div>
