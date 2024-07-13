8485ab25054c14d81af6caf2701482c7746f63fc
Ivaylo Stoyanov <ipstoyanov20@codingburgas.bg>
2024-07-13 17:04:22 +0300

    edit site

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 export default function Home() {
   return (
-		<main className="flex min-h-screen flex-col items-center ">
+		<main className="flex min-h-screen flex-col items-center overflow-hidden">
 			<Navbar />
 
 			<div className="hero grid place-items-start place-content-center bg-background min-h-screen">
@@ -26,6 +26,7 @@ export default function Home() {
 							className="z-0 max-w-sm  rounded-lg border-2 border-black shadow-bottom absolute"
 						/>
 					</div>
+
 					<div className="grid place-items-center place-content-start">
 						<p className="font-bold text-[#B59861] text-xl">
 							Unchanged. It was popularised in the 1960s with the release
@@ -39,26 +40,47 @@ export default function Home() {
 							<br />
 							excepturi exercitationem quasi. In deleniti eaque aut repudiandae
 						</p>
-
-						<button className="relative after:content-[''] after:top-0 after:left-0 after:absolute after:w-full after:h-full after:shadow-lg w-36 h-10 shadow-inner-top grid place-content-center shadow-white border border-black rounded-sm px-4 py-3 m-10 bg-[#B59861] text-[#f3ecdd] font-bold font-grotesk ">
-							Get Started
-						</button>
+						<div className="grid place-content-center place-items-center grid-cols-2 gap-0">
+							<button className="mr-0 relative after:content-[''] after:top-0 after:left-0 after:absolute after:w-full after:h-full after:shadow-lg w-36 h-10 shadow-inner-top grid place-content-center shadow-white rounded-sm px-4 py-3 m-10 bg-[#B59861] text-[#f3ecdd] font-bold font-grotesk ">
+								Order
+							</button>
+							<button className="mr-0 relative after:content-[''] after:top-0 after:left-0 after:absolute after:w-full after:h-full after:shadow-lg w-36 h-10 shadow-inner-top grid place-content-center shadow-white border border-[#B59861] rounded-sm px-4 py-3 m-10 bg-transparent text-[#B59861] font-bold font-grotesk ">
+								See reviews
+							</button>
+						</div>
 					</div>
 				</div>
 			</div>
 
 			<div className="hero bg-background min-h-screen">
-				<div className="hero-content flex-col lg:flex-row">
-					<img src="darbuka.png" className="max-w-sm rounded-lg shadow-2xl" />
-					<div>
-						<h1 className="text-5xl font-bold">Box Office News!</h1>
-						<p className="py-6">
-							Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-							excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-							et a id nisi.
-						</p>
-						<button className="btn btn-primary">Get Started</button>
-					</div>
+				<div className="w-screen h-screen px-10 py-0 flex-col lg:flex-row">
+					<h1 className="font-grotesk font-bold text-9xl text-center mb-10">GENRE</h1>
+					<section className="grid place-items-center place-content-center grid-cols-3 translate-y-[100%]">
+						<div>
+							<h1 className="text-5xl font-bold font-grotesk text-center">BULGARIAN FOLK</h1>
+							<p className="py-6">
+								Provident cupiditate voluptatem et in. Quaerat fugiat ut
+								assumenda excepturi exercitationem quasi. In deleniti eaque aut
+								repudiandae et a id nisi.
+							</p>
+						</div>
+						<div>
+							<h1 className="text-5xl font-bold font-grotesk text-center">POP FOLK</h1>
+							<p className="py-6">
+								Provident cupiditate voluptatem et in. Quaerat fugiat ut
+								assumenda excepturi exercitationem quasi. In deleniti eaque aut
+								repudiandae et a id nisi.
+							</p>
+						</div>
+						<div>
+							<h1 className="text-5xl font-bold font-grotesk text-center">ARABIAN</h1>
+							<p className="py-6">
+								Provident cupiditate voluptatem et in. Quaerat fugiat ut
+								assumenda excepturi exercitationem quasi. In deleniti eaque aut
+								repudiandae et a id nisi.
+							</p>
+						</div>
+					</section>
 				</div>
 			</div>
 
