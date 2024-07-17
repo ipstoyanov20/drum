6d69a332b87f1f601dedf0d6e2263edbe0581d70
Ivaylo Stoyanov <ipstoyanov20@codingburgas.bg>
2024-07-17 19:03:49 +0300

    Fix small phones

--- app/page.tsx
+++ app/page.tsx
@@ -98,10 +98,10 @@ export default function Home() {
 							excepturi exercitationem quasi. In deleniti eaque aut repudiandae
 						</p>
 						<div className="grid grid-col-1 grid-rows-2 sm:grid-cols-2 gap-0">
-							<button className="ml-0 relative after:content-[''] after:top-0 after:left-0 after:absolute after:w-full after:h-full after:shadow-lg w-96 sm:w-36 from-ex:max-to-ex:ml-2 from-ex:max-to-ex:w-80 h-10 shadow-inner-top grid place-content-center shadow-white rounded-sm px-4 py-3 mb-0 sm:m-3 bg-[#B59861] text-[#f3ecdd] font-bold font-grotesk ">
+							<button className="ml-0 relative after:content-[''] after:top-0 after:left-0 after:absolute after:w-full after:h-full after:shadow-lg w-96 sm:w-36  h-10 shadow-inner-top grid place-content-center shadow-white rounded-sm px-4 py-3 mb-0 sm:m-3 bg-[#B59861] text-[#f3ecdd] font-bold font-grotesk ">
 								ORDER
 							</button>
-							<button className="ml-0 whitespace-nowrap  relative after:content-[''] after:top-0 after:left-0 after:absolute after:w-full after:h-full after:shadow-lg w-96 from-ex:max-to-ex:ml-2 from-ex:max-to-ex:w-80 sm:w-36 h-10 shadow-inner-top grid place-content-center shadow-white border border-[#B59861] rounded-sm px-4 py-3 mt-3 sm:m-3 bg-transparent text-[#B59861] font-bold font-grotesk ">
+							<button className="ml-0 whitespace-nowrap  relative after:content-[''] after:top-0 after:left-0 after:absolute after:w-full after:h-full after:shadow-lg w-96  sm:w-36 h-10 shadow-inner-top grid place-content-center shadow-white border border-[#B59861] rounded-sm px-4 py-3 mt-3 sm:m-3 bg-transparent text-[#B59861] font-bold font-grotesk ">
 								SEE INTERVIEWS
 							</button>
 						</div>
@@ -110,11 +110,11 @@ export default function Home() {
 			</div>
 
 			<div className="hero bg-background min-h-screen">
-				<div className="w-screen h-screen px-10 py-0 flex-col lg:flex-row mt-[-40%] sm:mt-0">
+				<div className="w-screen h-screen px-10 py-0 flex-col lg:flex-row mt-[-50%] sm:mt-0">
 					<h1 className="font-grotesk font-bold from-ex:max-to-ex:text-5xl text-7xl sm:text-9xl text-center">
 						<span className="text-[#B59861] m-0">GENRES</span> <br /> I PLAY
 					</h1>
-					<section className="from-ex:max-to-ex:mt-[-150%]  mt-[-60%] sm:mt-0 grid place-items-center place-content-center md:max-lg:grid-cols-2 grid-cols-1 sm:grid-cols-6 gap-3 gap-y-5 ml-2 sm:-ml-4 translate-y-[30%]">
+					<section className="from-ex:max-to-ex:mt-[-100%] mt-[-90%] sm:mt-0 grid place-items-center place-content-center md:max-lg:grid-cols-2 grid-cols-1 sm:grid-cols-6 gap-3 gap-y-5 ml-2 sm:-ml-4 translate-y-[30%]">
 						<div className="shadow-bottom col-span-2 shadow-black border-2 border-black rounded-lg">
 							<h1 className="md-max-lg:text-xl text-3xl sm:text-5xl font-bold font-grotesk text-left p-3 flex flex-row justify-between items-center">
 								BG FOLK
@@ -182,7 +182,7 @@ export default function Home() {
 				</div>
 			</div>
 
-			<div className="w-screen h-screen p-10 from-ex:max-to-ex:mt-[250%] mt-[140%] sm:mt-0 sm:mb-[40%]">
+			<div className="w-screen h-screen p-10 from-ex:max-to-ex:mt-[160%] mt-[130%] sm:mt-0 from-ex:max-to-ex:mb-[20%] sm:mb-[50%]">
 				<h1 className="font-grotesk font-bold from-ex:max-to-ex:text-5xl text-7xl mb-5 sm:text-9xl text-center">
 					HOW IT <br /> WORKS?
 				</h1>
@@ -190,15 +190,15 @@ export default function Home() {
 					<ul className="grid place-items-center place-content-center relative grid-cols-2 gap-x-48">
 						<img
 							src={blobImg.src}
-							className="absolute from-ex:max-to-ex:translate-x-[-40%] from-ex:max-to-ex:translate-y-[-120%] translate-x-[-40%] translate-y-[-50%] sm:translate-x-[-130%] sm:translate-y-[-70%] scale-50"
+							className="absolute from-ex:max-to-ex:translate-x-[-40%] from-ex:max-to-ex:translate-y-[-100%] translate-x-[-40%] translate-y-[-50%] sm:translate-x-[-130%] sm:translate-y-[-70%] scale-50"
 							alt=""
 						/>
 						<img
 							src={blobImg.src}
-							className="absolute from-ex:max-to-ex:translate-x-[50%] from-ex:max-to-ex:translate-y-[-20%] translate-x-[40%] translate-y-[-160%] sm:translate-x-[-20%] sm:translate-y-[70%] scale-50"
+							className="absolute from-ex:max-to-ex:translate-x-[40%] from-ex:max-to-ex:translate-y-[-20%] translate-x-[40%] translate-y-[-160%] sm:translate-x-[-20%] sm:translate-y-[70%] scale-50"
 							alt=""
 						/>
-						<div className="ml-[350%] mt-[-600%] from-ex:max-to-ex:ml-[600%] from-ex:max-to-ex:mt-[-600%]  from-ex:max-to-ex:scale-[40%] sm:ml-0 sm:mt-0 scale-50 sm:scale-100 z-0 shadow-bottom w-[600px] h-[500px] relative rounded-t-[5%] rounded-b-[5%] border-2 overflow-hidden border-black ">
+						<div className="ml-[350%] mt-[-600%] from-ex:max-to-ex:ml-[400%] from-ex:max-to-ex:mt-[-400%]  from-ex:max-to-ex:scale-[40%] sm:ml-0 sm:mt-0 scale-50 sm:scale-100 z-0 shadow-bottom w-[600px] h-[500px] relative rounded-t-[5%] rounded-b-[5%] border-2 overflow-hidden border-black ">
 							<span
 								style={{
 									transform: `translateX(${positionLeft}%)`,
@@ -221,7 +221,7 @@ export default function Home() {
 								))}
 							</span>
 						</div>
-						<div className="grid grid-cols-2 grid-rows-2 sm:grid-rows-4 sm:grid-cols-1 ml-[-450%] from-ex:max-to-ex:ml-[-750%] sm:ml-0 from-ex:max-to-ex:translate-y-[30%]  translate-y-[25%] sm:translate-y-0 from-ex:max-to-ex:scale-[50%] scale-[60%] sm:scale-100 gap-x-48 sm:gap-x-0">
+						<div className="grid grid-cols-2 grid-rows-2 sm:grid-rows-4 sm:grid-cols-1 ml-[-450%] from-ex:max-to-ex:gap-x-60 from-ex:max-to-ex:ml-[-550%] sm:ml-0 from-ex:max-to-ex:translate-y-[40%]  translate-y-[25%] sm:translate-y-0 scale-[60%] sm:scale-100 gap-x-48 sm:gap-x-0">
 							{items.map((item, index) => (
 								<li
 									key={item.id}
