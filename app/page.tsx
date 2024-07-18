c9c1a25515e998485eb62d727da3d76af6a9e434
Ivaylo Stoyanov <ipstoyanov20@codingburgas.bg>
2024-07-18 08:31:07 +0300

    caps on title in hero section

--- app/page.tsx
+++ app/page.tsx
@@ -66,7 +66,7 @@ export default function Home() {
 		<main className="flex min-h-screen flex-col items-center overflow-x-hidden">
 			<div className="hero grid place-items-start place-content-center bg-background min-h-screen mt-[-30%] sm:mt-0">
 				<div className="flex-col lg:flex-row-reverse">
-					<div className="top-[20%] left-[5%] absolute lg:block hidden">
+					<div className="top-[12%] left-[5%] absolute lg:block hidden">
 						<div className="border-2 absolute top-[95%] left-[-10%] rounded-lg bg-white border-black shadow-bottom w-48 h-10 font-grotesk font-bold grid place-content-center place-items-center">
 							<p>See on tiktok</p>
 						</div>
@@ -75,7 +75,7 @@ export default function Home() {
 							className="z-0 max-w-sm border-2 border-black shadow-bottom rounded-lg"
 						/>
 					</div>
-					<div className="top-[45%] left-[75%] absolute lg:block hidden">
+					<div className="top-[55%] left-[75%] absolute lg:block hidden">
 						<div className="border-2 absolute z-10 translate-x-[120%] -translate-y-[40%] bg-white rounded-lg border-black shadow-bottom w-48 h-10 font-grotesk font-bold grid place-content-center place-items-center">
 							<p>See on tiktok</p>
 						</div>
@@ -90,7 +90,7 @@ export default function Home() {
 							Unchanged. It was popularised in the 1960s with the release
 						</p>
 						<h1 className="z-[10] from-ex:max-to-ex:text-5xl text-7xl font-bold sm:font-bold text-left font-grotesk m-5 sm:text-center sm:text-9xl">
-							My Rythm <br /> Your favourite song
+							MY RITHM <br /> YOUR FAVOURITE SONG
 						</h1>
 						<p className="m-5 grid place-content-center text-left sm:text-center from-ex:max-to-ex:text-sm text-lg text-[#B59861] sm:text-base">
 							Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
