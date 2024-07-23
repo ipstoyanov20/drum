f0efc4cb1d01adfc2f818ae5c9857b10d98f3cec
Ivaylo Stoyanov <ipstoyanov20@codingburgas.bg>
2024-07-23 12:39:04 +0300

    Optimize Spline

--- components/order.tsx
+++ components/order.tsx
@@ -2,6 +2,8 @@
 import React from 'react'
 import Spline from '@splinetool/react-spline/next';
 import OrderForm from './order-form';
+import darbuka3D from '../public/3d-darbuka.png';
+import Image from 'next/image';
 function Order() {
     
   return (
@@ -11,8 +13,14 @@ function Order() {
 			</h1>
 			<div className="w-full h-full flex justify-between items-start p-10 sm:flex-row flex-col">
 				<div className="p-5 bg-[#B59861] shadow-md from-ex:max-to-ex:scale-[50%] w-96 h-[27rem] sm:h-[27rem] sm:w-[27rem] scale-75 sm:scale-100 from-ex:max-to-ex:ml-[-10%] from-ex:max-to-ex:mt-[-30%] mt-[-10%] sm:mt-0 ml-[-5%] sm:ml-[10%] animate-blob transition-all ease-in-out">
+					<Image
+					 src={darbuka3D}
+					 alt="darbuka-3d"
+					 className='lg:hidden block absolute scale-150 top-[25%] left-[0%] w-full h-full'
+					 style={{ width: 'auto', height: 'auto' }}
+					 />
 					<Spline
-						className="cursor-pointer w-full h-full"
+						className="cursor-pointer lg:block hidden w-full h-full"
 						scene="https://prod.spline.design/7c944-ndpVCUK5gj/scene.splinecode"
 					/>
 				</div>
