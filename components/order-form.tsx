5082d0e2656cd1c447c5cbc27ca3b30623a00dc2
Ivaylo Stoyanov <ipstoyanov20@codingburgas.bg>
2024-09-09 21:22:32 +0300

    Polish order-form

--- components/order-form.tsx
+++ components/order-form.tsx
@@ -73,102 +73,104 @@ function OrderForm() {
     };
 
     return (
-        <form
-            data-aos="fade-left"
-            action="/api/checkout_sessions"
-            method="POST"
-            onSubmit={handleSubmit}
-            className="w-full h-full p-5 sm:p-10 from-ex:max-to-ex:mt-[-20%] mt-[0%] sm:mt-0 from-ex:max-to-ex:mb-[20%]"
-        >
-            <div className="flex flex-col sm:flex-row gap-0 sm:gap-5 w-full">
-                <div className="mb-4">
-                    <label
-                        htmlFor="email"
-                        className="block text-sm font-grotesk font-bold text-gray-700"
-                    >
-                        {t('email')}
-                    </label>
-                    <input
-                        type="email"
-                        name="email"
-                        id="email"
-                        required
-                        className="mt-1 block w-full px-4 py-3 bg-transparent shadow-bottom rounded-md border-black border-2"
-                        />
-                </div>
-                <div className="mb-4">
-                    <label
-                        htmlFor="phone"
-                        className="block text-sm font-grotesk font-bold text-gray-700"
-                        >
-                        {t('phone')}
-                    </label>
-                    <PhoneInput
-                        country={'bg'}
-                        value={``}
-                        inputProps={{
-                            required: true,
-                            name: "phone"
-                        }}
-                        inputStyle={{
-                            marginTop: "0.25rem",
-                            display: "block",
-                            width: "100%",
-                            height: "3.5rem",
-                            padding: "1rem 3rem",
-                            backgroundColor: "transparent",
-                            borderRadius: "0.375rem",
-                            borderColor: "black",
-                            borderWidth: "2px",
-                            boxShadow: "3px 5px 0 0px black",
-                          }}
-                        // id="phone"
-                        // required
-                        />
-                </div>
-            </div>
-            <div className="mb-4 flex-1">
-                <label
-                    htmlFor="YTlink"
-                    className="block text-sm font-grotesk font-bold text-gray-700"
-                    >
-                    {t('ytlink')}
-                </label>
-                <input
-                    type="text"
-                    name="YTlink"
-                    id="YTlink"
-                    required
-                    placeholder={`https://www.youtube.com/watch?v=VIDEO_ID`} 
-                    className="mt-1 block w-full px-4 py-3 bg-transparent rounded-md border-black border-2 shadow-bottom"
-                    />
-            </div>
-            <div className="mb-4">
-                <label
-                    htmlFor="additionalInfo"
-                    className="block text-sm font-grotesk font-bold text-gray-700"
-                    >
-                    {t('instructions')}
-                </label>
-                <textarea
-                    name="additionalInfo"
-                    id="additionalInfo"
-                    rows={7}
-                    placeholder={t('instructions-placeholder')}
-                    className="mt-1 block w-full px-4 py-3 bg-transparent rounded-md border-black border-2 shadow-bottom"
-                ></textarea>
-            </div>
-            <input type="hidden" name="locale" value={locale} />
-            <button
-                type="submit"
-                role="link"
-				disabled={isSubmitting}
-                className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-grotesk font-medium rounded-md text-white bg-[#B59861] hover:bg-[#B59861]/90 transition-all duration-100"
-            >
-                {isSubmitting ? "Loading..." : t('button')}
-            </button>
-        </form>
-    );
+			<form
+				data-aos="fade-left"
+				action="/api/checkout_sessions"
+				method="POST"
+				onSubmit={handleSubmit}
+				className="w-full h-full p-5 sm:p-10 from-ex:max-to-ex:mt-[-20%] mt-[0%] sm:mt-0 from-ex:max-to-ex:mb-[20%]"
+			>
+				<div className="flex flex-col sm:flex-row gap-0 sm:gap-5 w-full">
+					<div className="mb-4 flex-1">
+						<label
+							htmlFor="email"
+							className="block text-sm font-grotesk font-bold text-gray-700"
+						>
+							{t("email")}
+						</label>
+						<input
+							type="email"
+							name="email"
+							id="email"
+							required
+							className="mt-1 block w-full px-4 py-3 bg-transparent shadow-bottom rounded-md border-black border-2 focus:border-2 focus:border-black focus:outline-none"
+						/>
+					</div>
+					<div className="mb-4 flex-1">
+						<label
+							htmlFor="phone"
+							className="block text-sm font-grotesk font-bold text-gray-700"
+						>
+							{t("phone")}
+						</label>
+						<PhoneInput
+							country={"bg"}
+							value={``}
+							inputProps={{
+								required: true,
+								name: "phone",
+							}}
+							inputStyle={{
+								marginTop: "0.25rem",
+								display: "block",
+								width: "100%",
+								height: "3.5rem",
+								padding: "1rem 3rem",
+								backgroundColor: "transparent",
+								borderRadius: "0.375rem",
+								borderColor: "black",
+								borderWidth: "2px",
+								boxShadow: "3px 5px 0 0px black",
+							}}
+                            inputClass="focus:border-2 focus:border-black focus:outline-none"
+
+							// id="phone"
+							// required
+						/>
+					</div>
+				</div>
+				<div className="mb-4 flex-1">
+					<label
+						htmlFor="YTlink"
+						className="block text-sm font-grotesk font-bold text-gray-700"
+					>
+						{t("ytlink")}
+					</label>
+					<input
+						type="text"
+						name="YTlink"
+						id="YTlink"
+						required
+						placeholder={`https://www.youtube.com/watch?v=VIDEO_ID`}
+						className="mt-1 block w-full px-4 py-3 bg-transparent rounded-md border-black border-2 shadow-bottom focus:border-2 focus:border-black focus:outline-none"
+					/>
+				</div>
+				<div className="mb-4">
+					<label
+						htmlFor="additionalInfo"
+						className="block text-sm font-grotesk font-bold text-gray-700"
+					>
+						{t("instructions")}
+					</label>
+					<textarea
+						name="additionalInfo"
+						id="additionalInfo"
+						rows={7}
+						placeholder={t("instructions-placeholder")}
+						className="mt-1 block w-full px-4 py-3 bg-transparent rounded-md border-black border-2 shadow-bottom focus:border-2 focus:border-black focus:outline-none"
+					></textarea>
+				</div>
+				<input type="hidden" name="locale" value={locale} />
+				<button
+					type="submit"
+					role="link"
+					disabled={isSubmitting}
+					className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-grotesk font-medium rounded-md text-white bg-[#B59861] hover:bg-[#B59861]/90 transition-all duration-100"
+				>
+					{isSubmitting ? "Loading..." : t("button")}
+				</button>
+			</form>
+		);
 }
 
 export default OrderForm;
